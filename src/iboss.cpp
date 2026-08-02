7f8a989414daece1009d5bc7e4eba114a75e80cf
Mohit Rathi <mohitrathi2490@gmail.com>
2026-08-02 13:40:57 +0530

    Fix: fixes repetition in IBOSS (partial_sort)

--- src/iboss.cpp
+++ src/iboss.cpp
@@ -1,7 +1,7 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <algorithm>
-#include <utility>
+#include <numeric>
 #include <omp.h>
 
 inline void IBOSS(const Eigen::MatrixXd &X, const Eigen::VectorXd &y, Eigen::MatrixXd &X_iboss, Eigen::VectorXd &y_iboss, int k, bool intercept) {
@@ -14,87 +14,57 @@ inline void IBOSS(const Eigen::MatrixXd &X, const Eigen::VectorXd &y, Eigen::Mat
     }
 
     k = std::min<int>(k, static_cast<int>(N));
-    int r = (intercept) ? k / (2 * (p - 1)) : k / (2 * p);
-    r = std::max(r, 1);
+    int num_features = intercept ? (p - 1) : p;
+    
+    int r = k / (2 * num_features);
+    if (r == 0) r = 1;
 
-    std::vector<char> global_sel(N, 0);
-    #pragma omp parallel
-    {
-        std::vector<size_t> local_sel;
-        local_sel.reserve(p * 2 * r / omp_get_num_threads() + 100);
+    int u = std::min<int>(k, 2 * num_features * r); 
 
-        std::vector<std::pair<double, size_t>> maximals;
-        std::vector<std::pair<double, size_t>> minimals;
-        maximals.reserve(r + 1);
-        minimals.reserve(r + 1);
+    std::vector<int> nums(N);
+    std::iota(nums.begin(), nums.end(), 0);
 
-        #pragma omp for schedule(dynamic)
-        for (Eigen::Index j = (intercept ? 1 : 0); j < p; ++j) {
+    int current_offset = 0;
 
-            maximals.clear();
-            minimals.clear();
+    for (Eigen::Index j = (intercept ? 1 : 0); j < p; ++j) {
+        if (current_offset >= u) break;
 
-            for (Eigen::Index i = 0; i < r; ++i) {
-                double val = X(i, j);
-                maximals.emplace_back(val, i);
-                minimals.emplace_back(val, i);
-            }
+        auto greater = [&X, j](int a, int b) { return X(a, j) > X(b, j); };
+        auto lesser = [&X, j](int a, int b) { return X(a, j) < X(b, j); };
 
-            std::make_heap(maximals.begin(), maximals.end(), std::greater<std::pair<double, size_t>>());
-            std::make_heap(minimals.begin(), minimals.end());
 
-            for (Eigen::Index i = r; i < N; ++i) {
-                double val = X(i, j);
 
-                if (val > maximals.front().first) {
-                    std::pop_heap(maximals.begin(), maximals.end(), std::greater<std::pair<double, size_t>>());
-                    maximals.back() = {val, i};
-                    std::push_heap(maximals.begin(), maximals.end(), std::greater<std::pair<double, size_t>>());
-                }
+        int r_max = std::min<int>(r, u - current_offset);
+        std::partial_sort(nums.begin() + current_offset, 
+                          nums.begin() + current_offset + r_max, 
+                          nums.end(), greater);
 
-                if (val < minimals.front().first) {
-                    std::pop_heap(minimals.begin(), minimals.end());
-                    minimals.back() = {val, i};
-                    std::push_heap(minimals.begin(), minimals.end());
-                }
-            }
+        current_offset += r_max;
 
-            for (const auto& kv : maximals) local_sel.push_back(kv.second);
-            for (const auto& kv : minimals) local_sel.push_back(kv.second);
-        }
 
-        #pragma omp critical
-        {
-            for(auto idx : local_sel) {
-                global_sel[idx] = 1;
-            }
-        }
+        
+        if (current_offset >= u) break;
+
+        int r_min = std::min<int>(r, u - current_offset);
+        std::partial_sort(nums.begin() + current_offset, 
+                          nums.begin() + current_offset + r_min, 
+                          nums.end(), lesser);
+
+        current_offset += r_min;
     }
 
-    std::vector<Eigen::Index> selected_indices;
-    selected_indices.reserve(k);
-
-    for(Eigen::Index i = 0; i < N; ++i) {
-        if(global_sel[i]) {
-            selected_indices.push_back(i);
-        }
-    }
-
-    Eigen::Index actual_k = selected_indices.size();
-    X_iboss.resize(actual_k, p);
-    y_iboss.resize(actual_k);
+    X_iboss.resize(current_offset, p);
+    y_iboss.resize(current_offset);
 
     #pragma omp parallel for schedule(static)
     for (Eigen::Index j = 0; j < p; ++j) {
-        for (Eigen::Index i = 0; i < actual_k; ++i) {
-            X_iboss(i, j) = X(selected_indices[i], j);
+        for (int i = 0; i < current_offset; ++i) {
+            X_iboss(i, j) = X(nums[i], j);
         }
     }
 
     #pragma omp parallel for schedule(static)
-    for (Eigen::Index i = 0; i < actual_k; ++i) {
-        y_iboss(i) = y(selected_indices[i]);
+    for (int i = 0; i < current_offset; ++i) {
+        y_iboss(i) = y(nums[i]);
     }
-
-    return;
 }
